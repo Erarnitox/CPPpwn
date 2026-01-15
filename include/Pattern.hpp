631734c21b52b6493fe1bc476160fdb4a537ac71
Erarnitox <49777103+Erarnitox@users.noreply.github.com>
2026-01-15 22:53:25 +0100

    Refactor namespace declaration in Pattern.hpp

--- include/Pattern.hpp
+++ include/Pattern.hpp
@@ -4,13 +4,11 @@
 #include <cstdint>
 #include <vector>
 
-namespace cpppwn
-{
-    
+namespace cpppwn {
     //Generates a cyclic pattern (De Bruijn sequence) to determine buffer overflow offsets. 
     std::string cyclic(size_t length, size_t period = 4);
 
-   //Finds the offset of a value inside the generated cyclic pattern.
+    //Finds the offset of a value inside the generated cyclic pattern.
     int cyclicFind(std::string const& subPattern, size_t period = 4);
     int cyclicFind(uint32_t value, size_t period = 4);
 }
